fc8da99c847da84c8b5153d5903698ee5cf783f9
peddadaanvitha <83341816+peddadaanvitha@users.noreply.github.com>
2021-07-09 16:40:31 +0530

    Create conditional2.c

---
+++ conditional2.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main(void)
+{
+	int n;
+	printf("Enter a random choice :");
+	scanf("%d",&n);
+	switch(n)
+	{
+		case 1:
+			printf("Food item - French Fries\nPrice - Rs 99");
+			break;
+		case 2:
+			printf("Food item - Sandwich\nPrice - Rs 149");
+			break;
+		case 3:
+			printf("Food item - Pizza\nPrice - Rs 239");
+			break;
+		case 4:
+			printf("Food item - Burger\nPrice - Rs 129");
+			break;
+		case 5:
+			printf("Food item - Pasta\nPrice - Rs 179");
+			break;
+		default:
+			printf("Invalid choice\n");
+			break;
+	}
+	return 0;
+}
